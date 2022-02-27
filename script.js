442c7c325d492c40e6cc97b6e2f5fac68137227a
Giden <gabrieldvolpi@gmail.com>
2022-02-27 01:38:11 -0300

    actualizado

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-//const saludo = "hola"
-//console.log(saludo)
+const nombre = document.getElementById('Nombre');
+const email = document.getElementById('Mail');
+const pass = document.getElementById('Pass');
+const boton = document.getElementById('btn-enviar');
+const resultado = document.querySelector(".resultado")
 
-const nombreDeUsuario = document.querySelector('#NombreDeUsuario').value;
-const MailDelusuario = document.querySelector('#MailDelusuario').value;
-const PassUsuario = document.querySelector('#PassUsuario').value;
+boton.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let error = validarCampos();
+	if (error[0]){
+		resultado.innerHTML = error[1];
+		resultado.classList.add("red");
+	}else {
+		resultado.innerHTML = "TODO OK";
+		resultado.classList.add("green");
+		resultado.classList.remove("red");
+	}
+})
 
-const submit = document.querySelector('#submit')
+const validarCampos = ()=>{
+	let error = [];
+	if (nombre.value.length < 5) {
+		error[0] = true;
+		error[1]="El Nombre No Puede Tener Menos de 5 Letras";
+		return error;
+	}else if(nombre.value.length > 40){
+		error[0] = true;
+		error[1]="El Nombre No Puede Tener Menos de 5 Letras";
+		return error;
+	}else if(email.value.length < 5 ||
+		email.value.length > 40 ||
+		email.value.indexOf("@") == -1 ||
+		email.value.indexOf(".") == -1){
+		error[0]=true;
+		error[1]="El Mail es Invalido";
+		return error;
+	}
 
-document.write(nombreDeUsuario + " " + MailDelusuario + " " + PassUsuario);
-console.log(nombreDeUsuario);
+	error[0]=false; 
+	return error;
+}
