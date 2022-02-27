9122cf06691c5c64b1760a308641fd75b02dfe3e
Giden <gabrieldvolpi@gmail.com>
2022-02-27 00:44:32 -0300

    actualizado

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 //const saludo = "hola"
 //console.log(saludo)
 
+const nombreDeUsuario = document.querySelector('#NombreDeUsuario').value;
+const MailDelusuario = document.querySelector('#MailDelusuario').value;
+const PassUsuario = document.querySelector('#PassUsuario').value;
+
+const submit = document.querySelector('#submit')
+
+document.write(nombreDeUsuario + " " + MailDelusuario + " " + PassUsuario);
+console.log(nombreDeUsuario);
